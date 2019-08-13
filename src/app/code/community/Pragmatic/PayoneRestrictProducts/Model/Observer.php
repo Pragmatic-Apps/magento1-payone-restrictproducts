5fa28faa66fc8885afabf1ff01bc24c11054b4f6
Julian <j.ereth@pragmatic-apps.de>
2019-08-13 13:18:37 +0200

    add feature to check for deviating shipping address

--- src/app/code/community/Pragmatic/PayoneRestrictProducts/Model/Observer.php
+++ src/app/code/community/Pragmatic/PayoneRestrictProducts/Model/Observer.php
@@ -16,6 +16,35 @@ class Pragmatic_PayoneRestrictProducts_Model_Observer
             if (in_array($_quote_method->getCode(), $_restricted_methods)) {
                 $_event_quote =  $event->getQuote();
                 $_quote = (!is_null($_event_quote)) ? $_event_quote :  Mage::getSingleton('checkout/cart')->getQuote();
+
+                // check deviating addresses
+                if (Mage::getStoreConfigFlag('payoneext_restrict/general/checkaddress')) {
+                    $oBillingAddress = $_quote->getBillingAddress()->getData();
+                    $oShippingAddress = $_quote->getShippingAddress()->getData();
+                    $addressDiff = array_diff([
+                        'firstname' => $oBillingAddress['firstname'],
+                        'lastname' => $oBillingAddress['firstname'],
+                        'company' => $oBillingAddress['company'],
+                        'street' => $oBillingAddress['street'],
+                        'postcode' => $oBillingAddress['postcode'],
+                        'city' => $oBillingAddress['city'],
+                        'country_id' => $oBillingAddress['country_id']
+                    ], [
+                        'firstname' => $oShippingAddress['firstname'],
+                        'lastname' => $oShippingAddress['firstname'],
+                        'company' => $oShippingAddress['company'],
+                        'street' => $oShippingAddress['street'],
+                        'postcode' => $oShippingAddress['postcode'],
+                        'city' => $oShippingAddress['city'],
+                        'country_id' => $oShippingAddress['country_id']
+                    ]);
+                    if ($addressDiff) {
+                        $event->getResult()->isAvailable = false;
+                        return;
+                    }
+                }
+
+                // check items
                 $_restricted_items = explode(',', Mage::getStoreConfig('payoneext_restrict/general/skipskus'));
                 foreach ($_quote->getAllItems() as $item)
                 {
